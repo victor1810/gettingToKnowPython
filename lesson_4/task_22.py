519417e13ff05506eaef7620876731e5c69a0a82
victor1810 <victor181078@gmail.com>
2023-09-15 16:16:45 +0300

    add

--- lesson_4/task_22.py
+++ lesson_4/task_22.py
@@ -3,3 +3,20 @@
 Пользователь вводит 2 числа. n — кол-во элементов первого множества.
 m — кол-во элементов второго множества. Затем пользователь вводит сами элементы множеств.'''
 
+def newset(num):
+    new_set = set()
+    for i in range(num):
+        new_set.add(int(input("Введите число для множества: ")))
+    return new_set
+
+n = int(input("Введите кол-во элементов первого множества: "))
+n_set = newset(n)
+
+m = int(input("Введите кол-во элементов второго множества: "))
+m_set = newset(m)
+
+print(*n_set)
+print(*m_set)
+
+s_set = sorted(n_set.intersection(m_set))
+print(*s_set)
--- lesson_4/task_24.py
+++ lesson_4/task_24.py
@@ -10,3 +10,17 @@
 для нахождения максимального числа ягод, которое может собрать за один заход собирающий модуль,
 находясь перед некоторым кустом заданной во входном файле грядки.'''
 
+n = int(input("Введите количество кустов: "))
+while n < 3:
+    dictionary = input("Вы ошиблись! Количество кустов не должно быть меньше 3-х! \nВведите количество кустов: ")
+
+numbers = list()
+for i in range(n):
+    numbers.append(int(input(f"Введите количество ягод на кусту {i + 1}: ")))
+
+numbers_count = list()
+for i in range(len(numbers) - 1):
+    numbers_count.append(numbers[i - 1] + numbers[i] + numbers[i + 1])
+numbers_count.append(numbers[-2] + numbers[-1] + numbers[0])
+
+print(f"Максимального числа ягод, которое может собрать за один заход собирающий модуль: {max(numbers_count)}")
